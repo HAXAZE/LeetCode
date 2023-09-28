afa518ca55f9e22fb2fe1322687230add31d376b
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2023-09-28 14:19:07 +0530

    Time: 3 ms (96.18%), Space: 17 MB (6.46%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -2,21 +2,36 @@ class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
 
-        int left = 0;
-        int right = nums.size()-1;
+        // Without Extra Space
+        // int left = 0;
+        // int right = nums.size()-1;
 
-        while(left<right){
-            if(nums[left]%2==0){
-                left++;
-            }
-            else if(nums[right]%2!=0){
-                right--;
-            }
-            else{
-                swap(nums[left],nums[right]);
-            }
+        // while(left<right){
+        //     if(nums[left]%2==0){
+        //         left++;
+        //     }
+        //     else if(nums[right]%2!=0){
+        //         right--;
+        //     }
+        //     else{
+        //         swap(nums[left],nums[right]);
+        //     }
+        // }
+        // return nums;
+        
+        //  With Extra Space
+        vector<int>evenNumbers;
+        vector<int>oddNumbers;
+        // separating even and odd numbers
+        for(int i =0;i<nums.size();i++){
+            if(nums[i]%2==0)
+                evenNumbers.push_back(nums[i]);
+            else
+                oddNumbers.push_back(nums[i]);
         }
-        return nums;
-
+        for(auto x:oddNumbers){
+            evenNumbers.push_back(x);
+        }
+        return evenNumbers;
     }
 };
