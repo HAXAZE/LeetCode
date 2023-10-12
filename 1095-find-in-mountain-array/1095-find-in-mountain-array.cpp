530dad1cf2660e17aad2f3736b90e183499340c9
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2023-10-12 14:33:14 +0530

    Time: 6 ms (44.79%), Space: 7.6 MB (6.90%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,60 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int length = mountainArr.length();
+
+        // using recursion
+
+        int peakIndex  = findpeakindex(1,length-2,mountainArr);
+        int increasingIndex = binarySearch(0,peakIndex,target,mountainArr,false);
+        if(mountainArr.get(increasingIndex)==target)
+            return increasingIndex;
+        // again using recursion 
+        int decreasingIndex = binarySearch(peakIndex + 1,length - 1,target,mountainArr,true);
+        if(mountainArr.get(decreasingIndex)==target)
+            return decreasingIndex;
+
+        return -1;
+    }
+
+    int findpeakindex(int low,int high,MountainArray &mountainArr){
+        while(low !=high){
+            int mid = low + (high - low)/2;
+            if(mountainArr.get(mid)<mountainArr.get(mid+1)){
+                low = mid+1;
+            }else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    int binarySearch(int low,int high,int target,MountainArray &mountainArr,bool reversed){
+        while(low!=high){
+            int mid = low + (high - low)/2;
+            if(reversed){
+                if(mountainArr.get(mid)>target)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
+            else{
+                if(mountainArr.get(mid)<target)
+                    low = mid+1;
+                else
+                    high = mid;
+            }
+        }
+        return low;
+    }
+};
