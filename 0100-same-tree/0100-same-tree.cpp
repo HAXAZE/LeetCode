c2622a57a97f8be961c4a8f7c367d7b61068f8ca
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2023-10-13 21:58:27 +0530

    Time: 2 ms (42.76%), Space: 11.5 MB (5.65%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,16 +11,35 @@
  */
 class Solution {
 public:
+    void inorder(TreeNode* root,vector<int>&v){
+        if(root!=NULL){
+            inorder(root->left,v);
+            v.push_back(root->val);
+            inorder(root->right,v);
+        }else
+            v.push_back(100000);
+    }
+    void preorder(TreeNode* root,vector<int>&v){
+        if(root!=NULL){
+            v.push_back(root->val);
+            preorder(root->left,v);
+            preorder(root->right,v);
+        }else
+            v.push_back(100000);
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // Trying Recursive
-        if(p == NULL && q == NULL) return true;
-        if(p == NULL) return false;
-        if(q == NULL) return false;
-        if(p->val !=  q->val) return false;
+       vector<int> inp,inq,prep,preq;
+       inorder(p,inp);
+       inorder(q,inq);
 
-        if(isSameTree(p->left,q->left) && isSameTree(p->right,q->right)){
-            return true;
-        }
-        return false;
+       preorder(p,prep);
+       preorder(q,preq);
+
+       if(inp == inq && prep == preq){
+           return true;
+       }
+       else{
+       return false;
+    }
     }
 };
