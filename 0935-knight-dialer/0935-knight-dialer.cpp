d72764f2a84820fb8f5655bbf664b10fe4e82ff9
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2023-11-27 22:32:24 +0530

    Time: 107 ms (67.69%), Space: 37.7 MB (41.67%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int knightDialer(int n) {
+        vector<vector<int>> jumps = {
+            {4, 6},
+            {6, 8},
+            {7, 9},
+            {4, 8},
+            {3, 9, 0},
+            {},
+            {1, 7, 0},
+            {2, 6},
+            {1, 3},
+            {2, 4}
+        };
+
+        int MOD = 1e9 + 7;
+        vector<vector<int>> dp(n, vector(10, 0));
+        for (int square = 0; square < 10; square++) {
+            dp[0][square] = 1;
+        }
+
+        for (int remain = 1; remain < n; remain++) {
+            for (int square = 0; square < 10; square++) {
+                int ans = 0;
+                for (int nextSquare : jumps[square]) {
+                    ans = (ans + dp[remain - 1][nextSquare]) % MOD;
+                }
+
+                dp[remain][square] = ans;
+            }
+        }
+
+        int ans = 0;
+        for (int square = 0; square < 10; square++) {
+            ans = (ans + dp[n - 1][square]) % MOD;
+        }
+
+        return ans;
+    }
+};
