548276398d315c71cc7ab8685949b6bd862de44b
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2024-02-04 11:03:33 +0530

    Time: 11 ms (16.70%), Space: 15.9 MB (24.81%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,18 +1,49 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int start = -1;
-        int last  = -1;
+    int start(vector<int>&arr,int target){
+        int s = 0;
+        int e = arr.size()-1;
+        int ans = -1;
 
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i]==target){
-                if(start ==-1){
-                    start = i;
-                }
-                last = i;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(target == arr[mid]){
+                ans = mid;
+                e = mid-1;
             }
+            else if(target<arr[mid]){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+            mid = s+(e-s)/2;
         }
-        return {start,last};
+        return ans;
+    }
 
+    int last(vector<int>&arr,int target){
+        int s= 0;
+        int e = arr.size()-1;
+        int ans = -1;
+
+        int mid = s+(e-s)/2;
+        while(s<=e){
+            if(target==arr[mid]){
+                ans = mid;
+                s = mid+1;
+            }
+            else if(target<arr[mid]){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {start(nums,target),last(nums,target)};
     }
 };
