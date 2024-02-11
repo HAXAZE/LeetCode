6aa7cc214203b5a3725cf7d8670adc0e616985a2
Sahil kumar <90493737+HAXAZE@users.noreply.github.com>
2024-02-11 16:16:12 +0530

    Time: 16 ms (48.08%), Space: 20.4 MB (25.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int sum  = 0;
-        for(int i = 0;i<n;i++){
-            sum +=nums[i];
-        }
-        int sum2 = 0;
-        for(int j = 1;j<=n;j++){
-            sum2 +=j;
-        }
+     int n = nums.size();
+     int sum = (n*(n+1))/2;
+     int tempsum = 0;
+     for(int i = 0;i<n;i++){
+         tempsum +=nums[i];
+     }
+     return sum - tempsum;
+}
 
-        return sum2 - sum;
-    }
 };
